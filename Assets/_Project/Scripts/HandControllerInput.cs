6a3a8d3bae03e9a900d8350510ae3620d5fc23fd
Vidhur Voora <lab.nirvana@gmail.com>
2017-10-29 17:36:33 -0700

    Initial Commit for Locomotion
    Initial Commit

---
+++ Assets/_Project/Scripts/HandControllerInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandControllerInput : MonoBehaviour {
+
+    public SteamVR_TrackedObject trackedObj;
+    public SteamVR_Controller.Device device;
+	// Use this for initialization
+	void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+        Debug.Log(device);
+        
+		if(device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        { //returns true as long as thhe button is pressed down
+            print("Controller trigger down");
+        }
+
+        if(device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            //returs true for the frame where the button was released
+            print("Controller trigger up");
+        }
+	}
+}
